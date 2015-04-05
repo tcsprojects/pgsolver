9fcba71bfcd26236b239449145f1b10a216c15a6
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-04-05 19:06:34 -0400

    Cleanup Zadeh

--- src/solvers/stratimpralgs/analyze_temp.mli
+++
@@ -1,4 +0,0 @@
-open Paritygame;;
-open Stratimpralgs;;
-
-val switch_zadeh_exp_tie_break_callback: int -> paritygame -> strategy -> game_valuation -> int array array -> int -> int -> string -> string -> unit
